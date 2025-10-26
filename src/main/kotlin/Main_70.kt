c18fd035e0dd189d876c3f6c057e7692625973b2
AidenChang <asdf62210@gmail.com>
2025-10-26 20:09:10 +0800

    LeetCode 70. 爬樓梯 (Climbing Stairs)
    init

---
+++ src/main/kotlin/Main_70.kt
@@ -0,0 +1,16 @@
+package com.aiden
+
+/**
+ * 題目：LeetCode 70. 爬樓梯 (Climbing Stairs)
+ * 類別：Dynamic Programming / Fibonacci
+ * 難度：簡單
+ * 說明：每次可以爬 1 或 2 階，計算爬到第 n 階有多少種方法
+ * 連結：https://leetcode.com/problems/climbing-stairs/
+ * 參考資料：
+ * https://www.youtube.com/watch?v=65oOtJzyJWY
+ * https://youtu.be/_TfB09416G8?si=8LK5pD8nuwE23LcL&t=1056
+ * https://www.stoody.com.tw/blog-detail/44
+ */
+fun main() {
+
+}
