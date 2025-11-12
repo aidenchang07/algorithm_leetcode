af2046730a5173f1cc36240e402ba343e987c695
AidenChang <asdf62210@gmail.com>
2025-11-12 12:47:37 +0800

    LeetCode 20. 有效的括號
    init

---
+++ src/main/kotlin/Main_20.kt
@@ -0,0 +1,12 @@
+package com.aiden
+
+/**
+ * 題目：LeetCode 20. 有效的括號
+ * 類別：String / Stack
+ * 難度：簡單
+ * 說明：判斷字串中的括號是否成對且順序正確，需符合左括號對應右括號、順序正確、無多餘括號等條件
+ * 連結：https://leetcode.com/problems/valid-parentheses/
+ */
+fun main() {
+
+}
